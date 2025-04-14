91c1268a2d7e13ea89fcc7a93515971b29a6c692
Craftbuilder <craftbuilder119@gmail.com>
2025-04-14 11:35:06 +0600

    debuggin...

--- CertifiedThrift/settings.py
+++ CertifiedThrift/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-%#5!q4=c+scfm@2hsslw5712)qh+!zr8+#%#_$@%*2+l3z3miv"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True``
+DEBUG = True
 
 ALLOWED_HOSTS = ['http://127.0.0.1:8000/', '127.0.0.1', '.onrender.com']
 
